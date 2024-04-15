025f7b0f5a726ffc3c0e1d5810d9194fb757ff10
Edward Wang <wang.huiyang@outlook.com>
2024-04-15 14:27:34 +0800

    fix: genesis block initialization

---
+++ Chain.js
@@ -0,0 +1,78 @@
+const Block = require('./Block')
+const Transaction = require('./Transaction')
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.__createGenesisBlock()]
+    this.difficulty = 2
+    this.pendingTransactions = []
+    this.miningReward = 100
+  }
+
+  __createGenesisBlock() {
+    return new Block(new Date().getTime(), [])
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1]
+  }
+
+  minePendingTransactions(minerRewardAddress) {
+    const block = new Block(new Date().getTime(), this.pendingTransactions)
+    block.mineBlock(this.difficulty)
+
+    console.log('Block mined: ', block.hash)
+
+    this.chain.push(block)
+
+    // 发放收益
+    this.pendingTransactions.push(new Transaction(null, minerRewardAddress, this.miningReward))
+
+    // this.pendingTransactions = [
+    //   new Transaction(null, minerRewardAddress, this.miningReward),
+    // ]
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction)
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0
+
+    for (const block of this.chain) {
+      for (const transaction of block.transactions) {
+        if (transaction.fromAddress === address) balance -= transaction.amount
+
+        if (transaction.toAddress === address) balance += transaction.amount
+      }
+    }
+
+    return balance
+  }
+
+  addBlock(block) {
+    block.previousHash = this.getLatestBlock().hash
+    block.mineBlock(this.difficulty)
+    this.chain.push(block)
+  }
+
+  isValidChain() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i]
+      const previousBlock = this.chain[i - 1]
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false
+      }
+
+      if (currentBlock !== previousBlock.hash) {
+        return false
+      }
+    }
+
+    return true
+  }
+}
+
+module.exports = Blockchain
