032ad70e2e02e84d721fb992fd6275787080e78d
Edward Wang <wang.huiyang@outlook.com>
2024-04-15 18:12:34 +0800

    feat: signature transaction

--- Block.js
+++ Block.js
@@ -12,21 +12,31 @@ class Block {
   calculateHash() {
     return SHA256(
       this.timestamp +
-      JSON.stringify(this.transactions) +
-      this.previousHash +
-      this.__nonce,
+        JSON.stringify(this.transactions) +
+        this.previousHash +
+        this.__nonce,
     ).toString()
   }
 
   mineBlock(difficulty) {
     while (
       this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
-      ) {
+    ) {
       this.hash = this.calculateHash()
       this.__nonce++
     }
 
     console.log('Block mined: ', this.hash)
+  }
+
+  hasValidTransactions() {
+    for (const tx of this.transactions) {
+      if (!tx.isValid()) {
+        return false
+      }
+    }
+
+    return true
   }
 }
 
